edf3336aa9273338cd0d073c12763146d9fb881f
Valter de Sales <vsalesjr2008@gmail.com>
2025-11-12 22:03:31 -0300

    Finishing user page

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -48,7 +48,5 @@ def user():
     return render_template('main/user.html', current_user=current_user)
 
 @main_bp.route('/user/<UserData_chosen>')
-def UserData(UserData_chosen=None): 
-    if not UserData_chosen:
-        UserData_chosen = '1'
+def UserData(UserData_chosen):
     return render_template('main/user.html', current_user=current_user, UserData_chosen=UserData_chosen)
